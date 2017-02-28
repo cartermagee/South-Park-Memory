fa4f2af8e6032787a53d9f74240ec8cf2490e1f6
carter magee <cartermagee01@gmail.com>
2017-02-28 07:12:01 -0500

    made it jive with mobile in landscape view

---
+++ src/js/app1.js
@@ -0,0 +1,109 @@
+(function() {
+  "use strict";
+
+  const memoryGameModule = function() {
+    let newArr = [];
+    var startTimestamp = null;
+    let startSI = null;
+
+    class MemoryGame {
+      constructor(tiles) {
+        this.deck = tiles;
+        this.shuffledDeck = this.shuffleCards(this.deck);
+        this.build();
+      }
+
+      build() {
+        const source = $('#tiles').html();
+        const template = Handlebars.compile(source);
+        const context = {
+          southPark: this.shuffledDeck
+        };
+        const html = template(context);
+        $('.grid').prepend(html);
+      }
+
+      shuffleCards(cards) {
+        let fullDeck = cards.concat(cards); //doubles the length of the array
+        let shuffledCards = fullDeck.sort(function(a, b) {
+          return 0.5 - Math.random();
+        });
+        return shuffledCards;
+      }
+    }
+
+    function bindEvents() {
+      $('.start').on('click', function() {
+        clearInterval(startSI);
+        startTimer(0);
+        clearScreen();
+        getData();
+      });
+
+      $('.grid').on('click', '.tile', function() {
+        $(this).children('.card').toggleClass('is-hidden');
+        let dataName = $(this).attr('data-name');
+        newArr.push(dataName);
+        console.log($(this));
+        if (newArr.length === 2) {
+          checkPair(dataName);
+        }
+      });
+    }
+
+    function checkPair(shit) {
+      console.log(newArr);
+      if (newArr[0] === newArr[1]) {
+        $(`.tile[data-name=${shit}]`).addClass('paired').removeClass('tile');
+        newArr = [];
+        youWin();
+      } else {
+        newArr = [];
+      }
+    }
+
+    function youWin() {
+      if ($('.tile').length === 0) {
+        $('.win-screen').removeClass('is-hidden');
+        $('.win-screen').on('click', '.re-start', function() {
+          clearScreen();
+        });
+      }
+    }
+
+    function clearScreen() {
+      $('.grid').html('');
+      $('.win-screen').addClass('is-hidden');
+    }
+
+    function startTimer(myStartTimestamp) {
+      startTimestamp = parseInt(myStartTimestamp);
+      startSI = setInterval(function() {
+        startTimestamp++;
+        $('#timer').html(moment.unix(startTimestamp).format('mm:ss'));
+      }, 1000);
+    }
+
+    function init() {
+      bindEvents();
+    }
+
+    function getData() {
+      const http = new XMLHttpRequest();
+      http.onreadystatechange = function() {
+        if (http.readyState === 4 && http.status === 200) {
+          const data = JSON.parse(http.response);
+          new MemoryGame(data);
+        }
+      };
+      http.open('GET', './data/southpark.json', true);
+      http.send();
+    }
+    return {
+      getData: getData,
+      init: init
+    };
+  };
+  const memoryApp = memoryGameModule();
+  memoryApp.init();
+})();
